3ba5f48020c92933215b084af56f986bccba6af8
Mutua <mutua@arifu.com>
2022-10-26 16:59:21 +0300

    Added few changes

--- streamlit_app/main.py
+++ streamlit_app/main.py
@@ -12,3 +12,25 @@ import streamlit as st
 st.write("DB username:", st.secrets["db_username"])
 st.write("DB password:", st.secrets["db_password"])
 st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+
+import os
+
+st.write(
+    "Has environment variables been set:",
+    os.environ["db_username"] == st.secrets["db_username"],
+)
+
+st.write(
+    "Has environment variables been set:",
+    os.environ["db_username"] == st.secrets["db_username"],
+)
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GCP_PROJECT_ID = os.getenv('GCP_PROJECT_ID')
+SERVICE_ACCOUNT_FILE = os.getenv('SERVICE_ACCOUNT_FILE')
+STORAGE_BUCKET_NAME = os.getenv('STORAGE_BUCKET_NAME')
+
+st.write(GCP_PROJECT_ID)
